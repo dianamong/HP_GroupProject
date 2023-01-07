83f9f12588d5ee3503d214b50d12e1ecf6c3dfbf
Sasha Sasha <sashamaninaa@mail.ru>
2023-01-07 21:15:04 +0300

    сделала слайдер

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,5 @@
-let rightArray = document.querySelector('arrayRight');
-let leftArray = document.querySelector('arrayLeft');
+
 let heroCards = document.querySelectorAll('.heroesBox__card');
-
-
 
 class HeroCard {
     constructor(img, name) {
@@ -15,44 +12,47 @@ let heroes = [
     new HeroCard("./assets/images/mainpage/lord.svg", 'Lord Voldemort'),
     new HeroCard("./assets/images/mainpage/hermione.svg", 'Hermione'),
     new HeroCard("./assets/images/mainpage/ron.svg", 'Ron Weasley'),
-
 ];
 
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        speed: 500,
+        easing: 'ease',
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
 
-document.addEventListener('DOMContentLoaded', function (event) {
-    let heroesContent = '';
-    heroesContent += ` <button class="heroesBox__btn heroesBox__btn_right" id="arrayRight"></button>
-        <div class="heroesBox__card"><img src="${heroes[0].img}" class="imgHover"alt="Harry">
-            <p class="galleryText_size">${heroes[0].name}</p>
-        </div>
-        <div class="heroesBox__card"><img src="${heroes[1].img}"class="imgHover" alt="Lord">
-            <p class="galleryText_size">${heroes[1].name}</p>
-        </div>
-        <div class="heroesBox__card"><img src="${heroes[2].img}" class="imgHover"alt="Hermione">
-            <p class="galleryText_size">${heroes[2].name}</p>
-        </div>
-        <div class="heroesBox__card"><img src="${heroes[3].img}"class="imgHover" alt="Ron">
-            <p class="galleryText_size">${heroes[3].name}</p>
-        </div>
-        <button class="heroesBox__btn heroesBox__btn_left" id="arrayLeft"></button>`
-    document.querySelector('.heroesBox2').innerHTML = heroesContent;
+        ]
+    });
+
 });
 
-//import Swiper, { Navigation, Pagination } from 'swiper';
-//import 'swiper/css';
-//import 'swiper/css/navigation';
-//import 'swiper/css/pagination';
-//const swiper = require('swiper');
 
-//const swiper = new Swiper('.heroesBox', {
-//modules: [Navigation, Pagination],
-//direction: 'horizontal',
-//loop: true,
-//navigation: {
-// nextEl: '.heroesBox__btn_left',
-// prevEl: '.heroesBox__btn_right',
-//},
-//});
+
 
 let instaFooter = document.getElementById('insta');
 let whatsFooter = document.getElementById('whats');
