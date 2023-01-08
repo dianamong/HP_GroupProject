f9c81c48547001a42f093b1f83cdebe2652a6cf2
Sasha Sasha <sashamaninaa@mail.ru>
2023-01-08 19:57:58 +0300

    модальное окно у героев

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -16,6 +16,7 @@ let heroes = [
 
 $(document).ready(function () {
     $('.slider').slick({
+        infinite: false,
         dots: false,
         arrows: true,
         slidesToShow: 4,
@@ -29,8 +30,8 @@ $(document).ready(function () {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
+                    slidesToScroll: 3
+
                 }
             },
             {
