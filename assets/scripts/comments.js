000238734e171ac4d0831155b7c824caa9d8fac7
DinaDST <for_work_Di@mail.ru>
2023-01-09 22:00:00 +0200

    add input-result

--- assets/scripts/comments.js
+++ assets/scripts/comments.js
@@ -1,7 +1,7 @@
 let comments = [];
+// loadComments();
 
 document.getElementById('comment-add').onclick = function() {
-    event.preventDefault();
     let commentName = document.getElementById('comment-name');
     let commentBody = document.getElementById('comment-body');
 
@@ -15,7 +15,6 @@ document.getElementById('comment-add').onclick = function() {
     commentBody.value = '';
 
     comments.push(comment);
-
     saveComments();
     showComments();
 }
@@ -24,24 +23,29 @@ function saveComments() {
     localStorage.setItem('comments', JSON.stringify(comments));
 }
 
+function loadComments() {
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
+
 function showComments() {
     let commentField = document.getElementById('comment-field');
     let out = '';
     comments.forEach(function(item) {
-        out += `<p class="text-right small">${timeConverter(item.time)}</p>`;
-    })
+
+        out += `<p class="comments__name" role="alert">${item.name}:</p>`;
+        out += `<p class="comments__content" role="alert">${item.body}</p>`;
+        out += `<p class="comments__time"><em>${timeConverter(item.time)}</em></p>`;
+    });
     commentField.innerHTML = out;
 }
 
 function timeConverter(UNIX_timestamp) {
     let a = new Date(UNIX_timestamp * 1000);
-    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Now', 'Dec'];
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     let year = a.getFullYear();
     let month = months[a.getMonth()];
     let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes();
-    let sec = a.getSeconds();
-    let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-    return time
+    let time = date + ' ' + month + ' ' + year;
+    return time;
 }
