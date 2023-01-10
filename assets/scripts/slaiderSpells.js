0f90d5d50f0391dd18d7243ade9487b9bd945c18
Sasha Sasha <sashamaninaa@mail.ru>
2023-01-11 01:17:51 +0300

    доделала заклинания наконец то

--- assets/scripts/slaiderSpells.js
+++ assets/scripts/slaiderSpells.js
@@ -14,8 +14,8 @@ $(document).ready(function () {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
+                    slidesToShow: 2,
+                    slidesToScroll: 2
 
                 }
             },
@@ -38,9 +38,3 @@ $(document).ready(function () {
     });
 });
 
-$('.btnSliderNext').on('click', function () {
-    $('.slider_spells').slick('slickNext');
-});
-$('.btnSliderPrev').on('click', function () {
-    $('.slider_spells').slick('slickPrev');
-});
--- assets/scripts/spells.js
+++ assets/scripts/spells.js
@@ -2,26 +2,102 @@ document.addEventListener("DOMContentLoaded", function (event) {
     fetch("https://hp-api.onrender.com/api/spells")
         .then(response => response.json())
         .then(spells => {
-            console.log(spells[0].name);
-
-            /*for (let spell of spells) {
-                document.querySelector('.api_spells').innerHTML = `<div class="slider__item slider__itemSpell">
-            <p class="galleryText_size">${spells[0].name} </p>
-        </div>`*/
-
-            let spellsArray = document.querySelectorAll('.slider__itemSpell');
-            spellsArray.forEach(elem => {
-                elem.addEventListener('click', () => {
-                    let modalSpells = $modal2({
-                        content: `<p class="spell-card__container__text1">Apparate</p><p class="spell-card__container__text2">A non-verbal transportation spell that allows a witch or wizard toinstantly travel on the spot and appear at another location (disapparate is the opposite)</p>`,
-                        footerButtons: [
-                            { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
-                        ]
-                    });
-                    modalSpells.show();
-                    document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide());
+            console.log(spells);
+            let spellsCards = document.querySelectorAll('.apiSpells');
+            spellsCards[0].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[61].name}</p><p class="spell-card__container__text2">${spells[61].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
                 })
-            });
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+            spellsCards[1].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[1].name}</p><p class="spell-card__container__text2">${spells[1].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+            spellsCards[2].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[17].name}</p><p class="spell-card__container__text2">${spells[17].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+
+
+            spellsCards[3].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[59].name}</p><p class="spell-card__container__text2">${spells[59].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+            spellsCards[4].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[23].name}</p><p class="spell-card__container__text2">${spells[23].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+            spellsCards[5].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[11].name}</p><p class="spell-card__container__text2">${spells[11].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+
+
+            spellsCards[6].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[8].name}</p><p class="spell-card__container__text2">${spells[8].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+            spellsCards[7].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[39].name}</p><p class="spell-card__container__text2">${spells[39].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
+            spellsCards[8].addEventListener('click', function () {
+                let modalSpells = $modal2({
+                    content: `<p class="spell-card__container__text1">${spells[14].name}</p><p class="spell-card__container__text2">${spells[14].description}</p>`,
+                    footerButtons: [
+                        { class: 'btn btn__cancel button__shine', text: 'Close', handler: 'modalHandlerCancel' }
+                    ]
+                })
+                modalSpells.show();
+                document.querySelector('.btn__cancel').addEventListener("click", () => modalSpells.hide())
+            })
         })
         .catch(err => console.log(err));
 })
