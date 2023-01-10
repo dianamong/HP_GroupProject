735cc2f99592eef7fcbfb7839979eb9c66d0d567
DinaDST <for_work_Di@mail.ru>
2023-01-10 15:47:08 +0200

    сделала новый див для результата

--- assets/scripts/comments.js
+++ assets/scripts/comments.js
@@ -1,51 +1,56 @@
 let comments = [];
 // loadComments();
 
-document.getElementById('comment-add').onclick = function() {
-    let commentName = document.getElementById('comment-name');
-    let commentBody = document.getElementById('comment-body');
+document.getElementById("comment-add").onclick = function() {
+    let commentName = document.getElementById("comment-name");
+    let commentBody = document.getElementById("comment-body");
 
     let comment = {
         name: commentName.value,
         body: commentBody.value,
-        time: Math.floor(Date.now() / 1000)
-    }
+        time: Math.floor(Date.now() / 1000),
+    };
+    const newElement = document.createElement("div");
+    newElement.innerHTML = `<p class="comments__name" role="alert">${commentName.value}:</p>
+    <p class="comments__content" role="alert">${commentBody.value}</p>
+    <p class="comments__time"><em>${timeConverter(Math.floor(Date.now() / 1000))}</em></p>
+    `;
+    const commentsWrapper = document.querySelector(".comments__result-item");
+    commentsWrapper.appendChild(newElement);
 
-    commentName.value = '';
-    commentBody.value = '';
+    commentName.value = "";
+    commentBody.value = "";
 
     comments.push(comment);
+
     saveComments();
     showComments();
-}
+};
 
 function saveComments() {
-    localStorage.setItem('comments', JSON.stringify(comments));
+    localStorage.setItem("comments", JSON.stringify(comments));
 }
 
 function loadComments() {
-    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    if (localStorage.getItem("comments")) comments = JSON.parse(localStorage.getItem("comments"));
     showComments();
 }
 
 function showComments() {
-    let commentField = document.getElementById('comment-field');
-    let out = '';
-    comments.forEach(function(item) {
-
-        out += `<p class="comments__name" role="alert">${item.name}:</p>`;
-        out += `<p class="comments__content" role="alert">${item.body}</p>`;
-        out += `<p class="comments__time"><em>${timeConverter(item.time)}</em></p>`;
-    });
-    commentField.innerHTML = out;
+    const commentsWrapper = document.querySelector(".comments__result-item");
+    console.log(commentsWrapper.children.length);
 }
 
 function timeConverter(UNIX_timestamp) {
     let a = new Date(UNIX_timestamp * 1000);
-    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     let year = a.getFullYear();
     let month = months[a.getMonth()];
     let date = a.getDate();
-    let time = date + ' ' + month + ' ' + year;
+    let time = date + " " + month + " " + year;
     return time;
 }
+
+const init = () => {
+    createElement();
+};
