4a6f362aede9b95b1e7be876fd3339173560d955
DinaDST <for_work_Di@mail.ru>
2023-01-18 21:44:00 +0200

    исправила ошибки

--- assets/scripts/commentsSlider.js
+++ assets/scripts/commentsSlider.js
@@ -8,20 +8,3 @@ btnEnd.onclick = () => {
 btnStart.onclick = () => {
     block.scrollBy(0, -120); // на 120 px вверх
 };
-
-$(document).ready(function() {
-    $.fn.animate_Text = function() {
-        let string = this.text();
-        return this.each(function() {
-            var $this = $(this);
-            $this.html(string.replace(/./g, '<span class="new">$&</span>'));
-            $this.find("span.new").each(function(i, el) {
-                setTimeout(function() {
-                    $(el).addClass("div_opacity");
-                }, 59 * i);
-            });
-        });
-    };
-    $("#plav-text").show();
-    $("#plav-text").animate_Text();
-});
